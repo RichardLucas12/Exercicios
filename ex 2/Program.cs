0bb751da1f448ac2d4b0c29b6b5445823fdaa3fb
RichardLucas12 <richardlucas.om12@gmail.com>
2020-11-24 16:31:41 -0300

    falta-so-o-sete

--- ex 2/Program.cs
+++ ex 2/Program.cs
@@ -18,36 +18,48 @@ namespace ex_2
 
              double precoa = 4.90*quant; 
              double precog = 5.30*quant;
-            if( combustivel == "alcool")
+
+            if( combustivel == "a")
             {
              
                 if(quant <= 20 )
                 {
-                    double desc3 = (3.0/100.0)*quant;
+                    double desc3 = (3.0/100.0)*precoa;
                     double result = precoa-desc3; 
-                    Console.WriteLine($"o valor é de {result} ");
+                    Console.WriteLine($"valor do desconto {desc3} ");
+                    Console.WriteLine($"total  {precoa} ");
+                    Console.WriteLine($"total a pagar {result} ");
                     
 
                 }else{
-                    double desc5 = (3.0/100.0)*quant;
+                    double desc5 = (5.0/100.0)*precoa;
                     double result = precoa-desc5; 
-                    Console.WriteLine($"o valor é de {result} ");
+                    Console.WriteLine($"o valor do desconto  {desc5} ");
+                    Console.WriteLine($"total {precoa} ");
+                    Console.WriteLine($"total a pagar  {result} ");
                 }
                 
-            }else {
+            }else if(combustivel == "g" ){
                 if(quant <= 20 )
                 {
-                    double desc4 = (4.0/100.0)*quant;
+                    double desc4 = (4.0/100.0)*precog;
                     double result = precog-desc4; 
-                    Console.WriteLine($"o valor é de {result} ");
+                    Console.WriteLine($"o valor do desconto {desc4} ");
+                    Console.WriteLine($"total   {precog} ");
+                    Console.WriteLine($"total a pagar  {result} ");
                     
 
                 }else{
-                    double desc6 = (6.0/100.0)*quant;
+                    double desc6 = (6.0/100.0)*precog;
                     double result = precog-desc6; 
-                    Console.WriteLine($"o valor é de {result} ");
+                    Console.WriteLine($"o valor do desconto {desc6} ");
+                    Console.WriteLine($"total   {precog} ");
+                    Console.WriteLine($"total a pagar  {result} ");
                 }
 
+            }else if( (combustivel !="g") && ( combustivel !="a" ) )
+            {
+                Console.WriteLine("--------invalido -------");
             }
 
         }
---
+++ ex6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior=0, menor, i=0, numero=0, tl=10;
+
+
+
+            Console.Write("Qual o 1º número? ");
+            numero = int.Parse(Console.ReadLine());
+            maior = numero;
+            menor = numero;
+
+            for(i = 2; i <= tl; i++){
+                 Console.Write("Qual o " + i +"º número? ");
+                numero = int.Parse(Console.ReadLine());
+
+            if(numero < menor){
+                menor = numero;
+                }
+            if (numero > maior){
+                maior = numero;
+                }
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("e o menor número é: " + menor);
+        }
+    }
+}
