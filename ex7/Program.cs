cdf5e36fc311c3fc25845e9f24dd7635eae91002
RichardLucas12 <richardlucas.om12@gmail.com>
2020-11-25 16:07:41 -0300

    finalizados

---
+++ ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numb = new int [16];
+
+            for( int i = 1; i < 16 ; i++)
+            {
+                Console.WriteLine($"Escreva um número:  ({16 - i} restante(s))");
+                numb[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n");
+
+            for( int j = 15; j > 0; j--)
+            {
+                Console.WriteLine( $"{numb[j]} \n \n  ");
+            }
+        }
+    }
+}
